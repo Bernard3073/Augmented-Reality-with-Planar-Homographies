fc4cc2ad874860b14d9d15d5ed8d1e0bdbc08a50
Bernard3073 <bert41003@gmail.com>
2022-01-08 11:43:10 -0500

    finish question 3.4

--- python/briefRotTest.py
+++ python/briefRotTest.py
@@ -1,18 +1,22 @@
 import numpy as np
 import cv2
 from matchPics import matchPics
-
+import matplotlib.pyplot as plt
+from helper import plotMatches
 
 #Q3.5
 #Read the image and convert to grayscale, if necessary
+cv_cover = cv2.imread('./data/cv_cover.jpg')
+hist = np.zeros(36)
 
 for i in range(36):
 	#Rotate Image
-	
+	cv_cover_rot = cv2.rotate(cv_cover, cv2.cv2.ROTATE_90_CLOCKWISE)
 	#Compute features, descriptors and Match features
-
+	matches, locs1, locs2 = matchPics(cv_cover, cv_cover_rot)
+	plotMatches(cv_cover, cv_cover_rot, matches, locs1, locs2)
 	#Update histogram
-
+	hist.append(matches)
 
 #Display histogram
-
+plt.hist()
--- python/matchPics.py
+++ python/matchPics.py
@@ -6,19 +6,24 @@ from helper import computeBrief
 from helper import corner_detection
 
 def matchPics(I1, I2):
+	ratio = 0.7
+	sigma = 0.15
 	#I1, I2 : Images to match
 	
 
 	#Convert Images to GrayScale
-	
+	I1_gray = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
+	I2_gray = cv2.cvtColor(I2, cv2.COLOR_BGR2GRAY)
 	
 	#Detect Features in Both Images
-	
+	locs1 = corner_detection(I1_gray, sigma)
+	locs2 = corner_detection(I2_gray, sigma)
 	
 	#Obtain descriptors for the computed feature locations
-	
+	desc1, locs1 = computeBrief(I1_gray, locs1)
+	desc2, locs2 = computeBrief(I2_gray, locs2)
 
 	#Match features using the descriptors
-	
+	matches = briefMatch(desc1, desc2, ratio)
 
 	return matches, locs1, locs2
