699d5b3b531dc2ad9fac1babe3f144d8a497d2fc
Bernard3073 <bert41003@gmail.com>
2022-01-12 16:00:15 -0500

    done

--- ec/ar_ec.py
+++ ec/ar_ec.py
@@ -31,21 +31,33 @@ def detect_feature(img1, img2):
     # find the keypoints and descriptors
     kp1, des1 = orb.detectAndCompute(img1, None)
     kp2, des2 = orb.detectAndCompute(img2, None)
-    # FLANN parameters
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)  # or pass empty dictionary
+    # # FLANN parameters
+    # FLANN_INDEX_KDTREE = 0
+    # index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    # search_params = dict(checks=50)  # or pass empty dictionary
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    des1 = np.float32(des1)
-    des2 = np.float32(des2)
-    matches = flann.knnMatch(des1, des2, k=2)
+    # flann = cv2.FlannBasedMatcher(index_params, search_params)
+    # des1 = np.float32(des1)
+    # des2 = np.float32(des2)
+    # matches = flann.knnMatch(des1, des2, k=2)
+    FLANN_INDEX_LSH = 6
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+                    table_number = 6, 
+                    key_size = 12,     
+                    multi_probe_level = 1)
+    search_params = dict(checks = 100)  # increase checks for better precision
+    matches = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = matches.knnMatch(des1, des2, k=2)
     # Apply ratio test
     good = []
-    for m, n in matches:
-        if m.distance < 0.8 * n.distance:
-            good.append(m)
-
+    # for m, n in matches:
+    #     if m.distance < 0.8 * n.distance:
+    #         good.append(m)
+    for m in matches:
+        if len(m) < 2:  # bad matches
+            continue
+        if m[0].distance < 0.6 * m[1].distance:
+            good.append(m[0])
     return kp1, kp2, good
 
 def main():
@@ -62,31 +74,55 @@ def main():
         book = loadVid('./data/book.mov')           # 641 frames 480x640
         np.save('./python/book.npy', book)
     cv_cover = cv2.imread('./data/cv_cover.jpg')
-    # Crop the top & bottom black bar
     frame0 = ar_source[0]
     x, y, w, h = cropBlackBar(frame0)
     frame0 = frame0[y:y+h, x:x+w]
-    # Crop left and right: only the central region is used for AR
     H, W = frame0.shape[:2]
     width = cv_cover.shape[1] * H / cv_cover.shape[0]
     wStart, wEnd = np.round([W/2 - width/2, W/2 + width/2]).astype(int)
-    frame0 = frame0[:, wStart:wEnd]
-    # Crop all frames in ar_source
     ar_source_crop = np.array([f[y:y+h, x:x+w][:, wStart:wEnd] for f in ar_source])
+
     cv_cover_gray = cv2.cvtColor(cv_cover, cv2.COLOR_BGR2GRAY)
+
+    # orb = cv2.ORB_create()
+    # FLANN_INDEX_LSH = 6
+    # index_params= dict(algorithm = FLANN_INDEX_LSH,
+    #                 table_number = 6, 
+    #                 key_size = 12,     
+    #                 multi_probe_level = 1)
+    # search_params = dict(checks = 100)  # increase checks for better precision
+    # matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    # cv_cover_gray = cv2.cvtColor(cv_cover, cv2.COLOR_BGR2GRAY)
+    # kp1, des1 = orb.detectAndCompute(cv_cover_gray, None)
+
     for i in range(len(ar_source_crop)):
         cv_book = book[i]
         cv_book_gray = cv2.cvtColor(cv_book, cv2.COLOR_BGR2GRAY)
         kp1, kp2, good = detect_feature(cv_cover_gray, cv_book_gray)
+        
+        # kp2, des2 = orb.detectAndCompute(cv_book_gray, None)
+        # matches = matcher.knnMatch(des1, des2, k=2)
+        # # Apply ratio test
+        # good = []
+        # for m in matches:
+        #     if len(m) < 2:  # bad matches
+        #         continue
+        #     if m[0].distance < 0.6 * m[1].distance:
+        #         good.append(m[0])
+
         feature_1 = []
         feature_2 = []
 
-        for i, match in enumerate(good):
-            feature_1.append(kp1[match.queryIdx].pt)
-            feature_2.append(kp2[match.trainIdx].pt)
+        # for i, match in enumerate(good):
+        for match in good:
+            feature_1.append([*kp1[match.queryIdx].pt])
+            feature_2.append([*kp2[match.trainIdx].pt])
         feature_1 = np.array(feature_1)
         feature_2 = np.array(feature_2)
         H, _ = cv2.findHomography(feature_1, feature_2, cv2.FM_RANSAC)
+        # locs1 = np.array([[*kp1[m.queryIdx].pt] for m in good])
+        # locs2 = np.array([[*kp2[m.trainIdx].pt] for m in good])
+        # H, _ = cv2.findHomography(locs1, locs2, cv2.RANSAC, 0.7)
         # print(H)
         template = cv2.resize(ar_source_crop[i], (cv_cover.shape[1], cv_cover.shape[0]))
         #Create mask of same size as template
